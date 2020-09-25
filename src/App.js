5bf6cea85de624c9cf189302419eb400946b8ce7
middtown <aarontm9@gmail.com>
2020-09-25 09:39:12 -0600

    under construction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../src/assets/under-construction.png";
+import logo from "./assets/under-construction.png";
 import "./App.css";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p class="under-construction">Currently under construction.</p>
+        <p>Under construction!</p>
       </header>
     </div>
   );
