2059afa803cd307bbbc74aa79013840dbd883e3d
middtown <aarontm9@gmail.com>
2020-09-27 14:12:38 -0600

    simple pages added for exploration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import logo from "./assets/under-construction.png";
 import "./App.css";
+import Homepage from "./pages/home/Homepage";
+import Nav from "./components/navigation/Nav";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Under construction!</p>
+    <div>
+      <header>
+        <Nav />
       </header>
+      <main className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Under construction!</p>
+        </header> */}
+        <Homepage />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <p>navigation</p>
+    </nav>
+  );
+};
+
+export default Nav;
